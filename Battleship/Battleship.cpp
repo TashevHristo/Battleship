196d740395a0f546d2e9d5521d7fa8fb71fc4762
TashevHristo <tashevhristo64@gmail.com>
2026-01-16 02:50:14 +0200

    Adding validations and computer's board

--- Battleship/Battleship.cpp
+++ Battleship/Battleship.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <Windows.h>
 
 const int SHIP_COUNT = 10;
@@ -31,7 +32,7 @@ void FillShips(int id, ShipCoordinates* ships, char** board) {
 		}
 	}
 }
-void PrintBoard(char** board) {
+void PrintBoard(char** player, char** computer) {
 	for (size_t i = 0; i < 11; i++)
 	{
 		if (i == 0) {
@@ -40,16 +41,30 @@ void PrintBoard(char** board) {
 			{
 				std::cout << j << "  ";
 			}
+			std::cout << '|';
+			for (size_t j = 0; j < 10; j++)
+			{
+				std::cout << "  " << j;
+			}
 			std::cout << std::endl;
 			continue;
 		}
-		for (size_t j = 0; j < 11; j++)
+		for (size_t j = 0; j < 22; j++)
 		{
 			if (j == 0) {
 				std::cout << i - 1;
 				continue;
 			}
-			std::cout << "  " << board[i - 1][j - 1];
+			else if (1 <= j && j < 11) {
+				std::cout << "  " << player[i - 1][j - 1];
+			}
+			else if (j == 11) {
+				std::cout << "  " << '|';
+			}
+			else {
+				std::cout << "  " << computer[i - 1][j - 12];
+			}
+			
 		}
 		std::cout << std::endl;
 	}
@@ -105,7 +120,29 @@ void ReadCoordinates(int first[COORDINATES_SIZE], int second[COORDINATES_SIZE], 
 		end[i] = second[i];
 	}
 }
-void EnterPlayerShips(ShipCoordinates* ships, char** board) {
+void UpdateShipCount(int length, int &one, int &two, int &three, int&four) {
+	switch (length) {
+	case 0:
+		one--;
+		break;
+	case 1:
+		two--;
+		break;
+	case 2:
+		three--;
+		break;
+	case 3:
+		four--;
+		break;
+	default:
+		break;
+	}
+}
+void ClearAndPrint(char** player, char** computer) {
+	system("cls");
+	PrintBoard(player, computer);
+}
+void EnterPlayerShips(ShipCoordinates* ships, char** player, char** computer) {
 	int oneSquare = 4;
 	int twoSquare = 3;
 	int threeSquare = 2;
@@ -120,9 +157,9 @@ void EnterPlayerShips(ShipCoordinates* ships, char** board) {
 
 		ReadCoordinatesFromConsole(firstCoordinates, secondCoordinates);
 		shipLenght = (secondCoordinates[0] - firstCoordinates[0]) + (secondCoordinates[1] - firstCoordinates[1]);
-		if (!isValidShip(firstCoordinates, secondCoordinates, shipLenght, board)) {
+		if (!isValidShip(firstCoordinates, secondCoordinates, shipLenght, player)) {
 			system("cls");
-			PrintBoard(board);
+			PrintBoard(player, computer);
 			std::cout << "Please Enter Valid Coordinates!" << std::endl;
 			continue;
 		}
@@ -130,26 +167,13 @@ void EnterPlayerShips(ShipCoordinates* ships, char** board) {
 		ReadCoordinates(firstCoordinates, secondCoordinates, ships[i].start, ships[i].end);
 		ships[i].length = shipLenght;
 		ships[i].isHorizontal = isHorizontal(firstCoordinates, secondCoordinates);
-		switch (shipLenght) {
-			case 0:
-				oneSquare--;
-				break;
-			case 1:
-				twoSquare--;
-				break;
-			case 2:
-				threeSquare--;
-				break;
-			case 3:
-				fourSquare--;
-				break;
-			default:
-				break;
-		}
-		system("cls");
-		FillShips(i, ships, board);
-		PrintBoard(board);
+		UpdateShipCount(shipLenght, oneSquare, twoSquare, threeSquare, fourSquare);
+		FillShips(i, ships, player);
+		ClearAndPrint(player, computer);
 	}
+}
+void EnterComputerShips(ShipCoordinates* ships, char** palyer, char** computer) {
+	
 }
 void FillBoard(char** board) {
 	for (size_t i = 0; i < 10; i++)
@@ -165,11 +189,14 @@ void FillBoard(char** board) {
 int main()
 {
 	//int** playerShips = new int*[SHIP_COUNT];
-	int* computerShips = new int[SHIP_COUNT] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	//int* computerShips = new int[SHIP_COUNT] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	ShipCoordinates* playerShips = new ShipCoordinates[SHIP_COUNT];
 	ShipCoordinates PlayerShips;
-	char** board = new char*[9];
-	FillBoard(board);
-	PrintBoard(board);
-	EnterPlayerShips(playerShips, board);
+	ShipCoordinates ComputerShips;
+	char** PlayerBoard = new char*[9];
+	char** ComputerBoard = new char*[9];
+	FillBoard(PlayerBoard);
+	FillBoard(ComputerBoard);
+	PrintBoard(PlayerBoard, ComputerBoard);
+	EnterPlayerShips(playerShips, PlayerBoard, ComputerBoard);
 }
